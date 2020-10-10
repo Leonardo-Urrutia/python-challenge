e919ab839b426776c6965c8f9ee40fc969590841
Leo <leonardorurrutia@gmail.com>
2020-10-10 14:05:37 -0700

    95% done with first activity

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,6 +5,7 @@ csvpath = os.path.join(os.path.abspath(__file__),'..', 'Resources', 'budget_data
 
 months = []
 profitLoss = 0
+profitList =[]
 with open(csvpath) as bankCsv:
     csvreader = csv.reader(bankCsv, delimiter=',')
     header = next(csvreader)
@@ -13,8 +14,30 @@ with open(csvpath) as bankCsv:
             splitDates = row[0].split("-")
             months = months + [splitDates[0]]
             profitLoss = profitLoss + float(row[1])
-
+            profitList.append(row[1])
 uniqueMonths = set(months)
 countOfMonths = len(uniqueMonths)
+# print(profitList)
+
+# Grabing the difference of current index subtracting by previous index
+# for loop starts at 1 to start looking at second line to allow subtracting the first index
+# end of range in loop is length of profitList
+
+profitChange = []
+singleChange = 0
+
+for i in range(1, len(profitList)):
+    singleChange = int(profitList[i]) - int(profitList[i - 1])
+    profitChange.append(singleChange)
+
+lengthOfProfitChange = len(profitChange)
+totalOfProfitChange = sum(profitChange)
+averageChange = round(totalOfProfitChange / lengthOfProfitChange, 2)
+maxChange = max(profitChange)
+minChange = min(profitChange)
+
 print(profitLoss)
 print(countOfMonths)
+print(averageChange)
+print(maxChange)
+print(minChange)
