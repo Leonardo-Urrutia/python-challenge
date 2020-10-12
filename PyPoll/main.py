5dafaecd3d100554a0c3d26070d42f9d05d93950
Leonardo R Urrutia <leonardorurrutia@gmail.com>
2020-10-11 20:16:06 -0700

    95% done!

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,7 +11,9 @@ csvpath = os.path.join(os.path.abspath(__file__),'..', 'Resources', 'election_da
 
 total_votes = 0
 allCandidatesWithDupes = []
-canidateTotals = {}
+candidateTotals = {}
+
+#Grabs count of total vote and fills dictionary with vote count for each candidate
 
 with open(csvpath) as pollCSV:
     csvreader = csv.reader(pollCSV, delimiter=',')
@@ -20,11 +22,37 @@ with open(csvpath) as pollCSV:
     for row in csvreader:
         total_votes += 1
         allCandidatesWithDupes.append(row[2])
-        canidateTotals[row[2]] = canidateTotals.setdefault(row[2], 0) + 1
+        candidateTotals[row[2]] = candidateTotals.setdefault(row[2], 0) + 1
 
-print(total_votes)
+
 uniqueCandidates = set(allCandidatesWithDupes)
 
-for candidate_name, vote_total in canidateTotals.items():
-    print(candidate_name)
-    print(vote_total)
+khanVoteCount = round(int(candidateTotals["Khan"]) / total_votes, 3)
+correyVoteCount = round(int(candidateTotals["Correy"]) / total_votes, 3)
+liVoteCount = round(int(candidateTotals["Li"]) / total_votes, 3)
+oTooleyVoteCount = round(int(candidateTotals["O'Tooley"]) / total_votes, 3)
+
+khanPercentage = "{:.0%}".format(khanVoteCount)
+correyPercentage = "{:.0%}".format(correyVoteCount)
+liPercentage = "{:.0%}".format(liVoteCount)
+oTooleyPercentage = "{:.0%}".format(oTooleyVoteCount)
+
+
+
+print(f'Election Results')
+print(f'Total Votes: {total_votes}')
+print(f'Khan: {khanPercentage} with {candidateTotals["Khan"]} votes')
+print(f'Correy: {correyPercentage} with {candidateTotals["Correy"]} votes')
+print(f'Li: {liPercentage} with {candidateTotals["Li"]}')
+print(f"""O'Tooley: {oTooleyPercentage} with {candidateTotals["O'Tooley"]}""")
+print(f'Winner: Khan')
+
+# print(total_votes)
+# print(khanVoteCount)
+# print(correyVoteCount)
+# print(liVoteCount)
+# print(oTooleykhanVoteCount)
+
+# for candidate_name, vote_total in canidateTotals.items():
+#     print(candidate_name)
+#     print(vote_total)
